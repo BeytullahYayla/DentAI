646f3610b878c221651a53a832d5119a3098c417
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-12-19 23:30:55 +0300

    DocStrings Added In Backend Server

--- backend/api/serve_model.py
+++ backend/api/serve_model.py
@@ -3,22 +3,43 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 from tensorflow.keras.applications.imagenet_utils import decode_predictions
-
+import cv2
 model = None
 
 
 def load_model_func():
-    model_path = 'E:\\UNI\\dentai\\backend\\model\\cross_validated_model_last.h5'
+    """
+    This function loads classification model which is saved before.
+    
+    Returns:
+        model(tf.keras.model): Classification model 
+    
+    
+    """
+    model_path = 'C:\\Users\\Beytullah\\Desktop\\dentai\\backend\\model\\cross_validated_model_last.h5'
     model = tf.keras.models.load_model(model_path)
     return model
 
 
 def predict(image: Image.Image):
+    """
+    This function predicts a series of dental conditions using the given image.
+
+    Parameters:
+        image (Image.Image) : An image of type Image.Image.
+    
+    Returns: 
+        results(dict): A dictionary containing the prediction results.
+    
+    """
+    
+    
     global model
     if model is None:
         model = load_model_func()
 
     image = np.asarray(image.resize((150, 150)))[..., :3]
+    
     image = np.expand_dims(image, 0)
     image = image / 255
 
@@ -56,5 +77,16 @@ def predict(image: Image.Image):
 
 
 def read_imagefile(file) -> Image.Image:
+    """
+    Reads an image file from BytesIO and returns it as a PIL Image.
+
+    Parameters:
+    - file: BytesIO
+        A BytesIO object containing the image file.
+
+    Returns:
+    Image.Image
+        A PIL Image object representing the image.
+    """
     image = Image.open(BytesIO(file))
     return image
