b70be47a3032c0002b384d724e541282a7e9febd
Mehmet Oğuz Özkan <83161954+MehmetOguzOzkan@users.noreply.github.com>
2024-01-06 13:26:23 +0300

    Update tables.py

--- backend/api/tables.py
+++ backend/api/tables.py
@@ -29,21 +29,3 @@ class Analyze(Base):
     hypodontia = Column(Double)
     created_at = Column(DateTime, default=datetime.utcnow())
 
-'''Analyze = Table(
-    'analyzes',
-    Base.metadata,
-    Column('id', CHAR(36), primary_key=True, default=uuid4, unique=True, index=True),
-    Column('user_id', CHAR(36), ForeignKey('users.id')),
-    Column('front_teeth_image', String(255)),
-    Column('intraoral_image', String(255)),
-    Column('diseases_id', Integer, ForeignKey('diseases.id')),
-    Column('created_at', DateTime, default=datetime.utcnow)
-)'''
-
-'''class Disease(Base):
-    __tablename__ = "diseases"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    description = Column(String(255), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)'''
